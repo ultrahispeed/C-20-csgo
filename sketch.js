53743d1f43b3cfd2bd571c67ba1ec2a6f53bd8a8
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2020-10-23 21:04:05 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,34 +1,40 @@
-var car, wall, speed, weight;
+var bullet, wall, speed, weight, thickness;
 
 function setup() {
   createCanvas(1600,400);
 
-  speed=random(55,90);
-  weight=random(400, 1500);
+  speed=random(223,321);
+  weight=random(30, 52);
+  thickness=random(22,83);
   
-  car = createSprite(50, 200, 50, 50);
-  car.velocityX=speed;
-  car.shapeColor=("white");
+  bullet = createSprite(50, 200, 50, 50);
+  bullet.velocityX=speed;
+  bullet.shapeColor=("white");
 
-  wall = createSprite(1500,200,60,height/2);
-  wall.shapecolor=("blue");
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapecolor=color(80,80,8);
 }
 
 function draw() {
   background("black"); 
-  
-  if(wall.x-car.x<(car.width+wall.width)/2){
-    car.velocityX=0
-    var deformation = 0.5*weight*speed*speed/22500;
-    if (deformation > 180){
-      car.shapeColor=("red");
-    }
-    if(deformation<180 && deformation > 100 ){
-      car.shapeColor=("yellow");
-    }
-    if(deformation<100){
-      car.shapeColor("green");
-    }
+if(hasCollided (bullet,wall)){
+  bullet.velocityX=0
+  var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+  if (damage>10){
+    wall.shapeColor=color(255,0,0);
   }
+  if (damage<10){
+    wall.shapeColor=color(0,255,0);
+  }
+}
   drawSprites();
+}
+
+function hasCollided(bullet1,wall1){
+  bulletRightEdge = bullet1.x+bullet1.width
+  wallLeftEdge = wall.x
+  if(bulletRightEdge>= wallLeftEdge){
+    return true;
+  }
+  return false;
 }
